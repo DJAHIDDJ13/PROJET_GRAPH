2e8f86333a03ac90dc087cf6e5f383fb6f889a86
Utilisateur <user@debian.arcahe.ovh>
2018-04-09 16:03:47 +0200

    added option to move nodes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var rawFile = new XMLHttpRequest();
 var data;
 var sommets = [];
 rawFile.overrideMimeType("application/json");
-rawFile.open("GET", "dataFixed.json", true);
+rawFile.open("GET", "dataFixed2.json", true);
 rawFile.onreadystatechange = function() {
 	if(rawFile.readyState == 4 && rawFile.status =="200"){
 		data = JSON.parse(rawFile.responseText);
@@ -34,7 +34,7 @@ function update() {
 		ctx.fillStyle = "#000000";
 		var dx = sommets[i].x;
 		var dy = sommets[i].y;
-		ctx.fillRect(x+zoom*dx, y+zoom*dy, zoom*5, zoom*5);
+		ctx.fillRect(x+zoom*dx-3, y+zoom*dy-3, zoom*6, zoom*6);
 	}
 }
 function zoomByVal(z){
@@ -42,9 +42,19 @@ function zoomByVal(z){
 	zoom = Math.min(2,Math.max(zoom - z/100, 0.2));
 	x = zoom/exZoom*(x-elem.width/2)+elem.width/2;
 	y = zoom/exZoom*(y-elem.height/2)+elem.height/2;
-	console.log(x,y);
-	console.log(zoom);
 	update();
+}
+
+function show(){
+	console.log("[");
+	for(var i=0; i<sommets.length; i++){
+		if(i!=sommets.length-1){
+			console.log("[",sommets[i].x,",",sommets[i].y,"],");
+		} else {
+			console.log("[",sommets[i].x,",",sommets[i].y,"]");
+		}
+	}
+	console.log("]");
 }
 window.addEventListener('load', function(){
 	if(imageLoad && dataLoad){
@@ -56,11 +66,21 @@ window.addEventListener('load', function(){
 			return;
 		ctx.drawImage(base_image, 0, 0, w, h);
 		var msDown = false;
+		var selected = -1;
 		var lastSeenAt = {x:null, y:null};
 		window.addEventListener("mousedown", function(e){
 			msDown = true;
 			lastSeenAt.x = e.clientX;
 			lastSeenAt.y = e.clientY;
+			for(var i=0; i<sommets.length; i++){
+				var sommet = sommets[i];
+				if(Math.sqrt(Math.pow(sommet.x-(e.clientX-x)*(1/zoom), 2) + Math.pow(sommet.y-(e.clientY-y)*(1/zoom),2)) < 5){
+					console.log("clicked on : ", sommet);
+					selected = i;
+					return;
+				}
+			}
+			selected = -1;
 			//console.log((e.clientX-x)*(1/zoom),(e.clientY-y)*(1/zoom));
 		},false);
 		window.addEventListener("mouseup", function(e){
@@ -75,12 +95,19 @@ window.addEventListener('load', function(){
 		},false);
 		elem.addEventListener("mousemove", function(e){
 			if(msDown){
-				x +=  e.clientX-lastSeenAt.x;
-				y +=  e.clientY-lastSeenAt.y;
-				
-				update();
-				lastSeenAt.x = e.clientX;
-				lastSeenAt.y = e.clientY;
+				if(selected != -1){
+					sommets[selected].x = (e.clientX-x)*(1/zoom);
+					sommets[selected].y = (e.clientY-y)*(1/zoom);
+					lastSeenAt.x = e.clientX;
+					lastSeenAt.y = e.clientY;
+					update();
+				} else {
+					x +=  e.clientX-lastSeenAt.x;
+					y +=  e.clientY-lastSeenAt.y;
+					update();
+					lastSeenAt.x = e.clientX;
+					lastSeenAt.y = e.clientY;
+				}
 			}
 		},false);
 	}
